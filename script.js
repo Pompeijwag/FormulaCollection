9f9bfbee60028f5b90714d7882c1b25169aeefd8
Shawn Wang <shawn2025@outlook.com>
2025-01-04 13:07:28 +0100

    search working

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible = user.header.toLowerCase().includes(value) || user.body.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+})
 
 fetch('./data.json')
     .then(res => res.json())
     .then(data => {
-        data.forEach(user => {
+        users = data.map(user => {
             const card = userCardTemplate.content.cloneNode(true).children[0]
             const header = card.querySelector("[data-header]")
             const formula = card.querySelector("[data-formula]")
@@ -15,6 +26,7 @@ fetch('./data.json')
             body.textContent = user.body
             userCardContainer.append(card)
             if (MathJax) MathJax.typesetPromise();
+            return {header: user.header, formula: user.formula, body: user.body, element: card}
         });
         
     })
