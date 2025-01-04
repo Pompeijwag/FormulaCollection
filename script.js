a62c3f536beb79ceea4efc1b943af1e1ea5d7c58
Shawn Wang <shawn2025@outlook.com>
2025-01-04 13:19:03 +0100

    changed convensions

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
+const formCardTemplate = document.querySelector("[data-form-template]")
+const formCardContainer = document.querySelector("[data-form-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
-let users = []
+let forms = []
 
 searchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
-    users.forEach(user => {
-        const isVisible = user.header.toLowerCase().includes(value) || user.body.toLowerCase().includes(value)
-        user.element.classList.toggle("hide", !isVisible)
+    forms.forEach(form => {
+        const isVisible = form.title.toLowerCase().includes(value) || form.body.toLowerCase().includes(value)
+        form.element.classList.toggle("hide", !isVisible)
     })
 })
 
 fetch('./data.json')
     .then(res => res.json())
     .then(data => {
-        users = data.map(user => {
-            const card = userCardTemplate.content.cloneNode(true).children[0]
-            const header = card.querySelector("[data-header]")
+        forms = data.map(form => {
+            const card = formCardTemplate.content.cloneNode(true).children[0]
+            const title = card.querySelector("[data-title]")
             const formula = card.querySelector("[data-formula]")
             const body = card.querySelector("[data-body]")
-            console.log(user)
-            header.textContent = user.header
-            formula.textContent = user.formula
-            body.textContent = user.body
-            userCardContainer.append(card)
+            console.log(form)
+            title.textContent = form.title
+            formula.textContent = form.formula
+            body.textContent = form.body
+            formCardContainer.append(card)
             if (MathJax) MathJax.typesetPromise();
-            return {header: user.header, formula: user.formula, body: user.body, element: card}
+            return {title: form.title, formula: form.formula, body: form.body, element: card}
         });
         
     })
